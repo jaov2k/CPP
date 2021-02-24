41de717ba438fac712914b484b790ff1bddec5d2
Jose Ortiz <jose@jaov2k.com>
2021-02-23 16:18:00 -0800

    init commit

---
+++ CIS014/Homework/Solutions/Homework02/Solution2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+/**
+ * FUNCTION NAME: canAlwaysWin
+ * PURPOSE: the function takes in the total of pennies from the user as input, 
+ *     computes its equivalent in dollars, quarters, dimes, nickels, and 
+ *     pennies, and outputs the result in string to the console
+ * PARAMETER:
+ *     int n
+ * RETURN VALUE:
+ *     string: result in dollars, quarters, dimes, nickels, and pennies
+ * FUNCTION SINGATURE:
+ *     string breakCash(int numPennies)
+ */ 
+
+bool canAlwaysWin(int n) {
+   // YOUR CODE HERE
+}
+int main() {
+   cout << canAlwaysWin(23034);
+   return 0;
+}
