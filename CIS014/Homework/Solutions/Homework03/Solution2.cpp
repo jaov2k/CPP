96af66d096035bd4f946a123d7d60a438cecf268
Jose Ortiz <jose@jaov2k.com>
2021-03-04 21:19:50 -0800

    initial commit

---
+++ CIS014/Homework/Solutions/Homework03/Solution2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+/* FUNCTION NAME: separateLetters
+ * PURPOSE:
+ *      To separate all of the 'a' and 'b' in the string so that all 'a' appear before 'b'
+ * PARAMETER:
+ *      string
+ * RETURN VALUE:
+ *      string
+ * FUNCTION SINGATURE:
+ *      string separateLetters(string input) */
+ 
+string separateLetters(string input)
+{
+    
+}
+
+int main() {
+    // your target function will be tested like so, with
+    // a random string input
+    cout << separateLetters("abababa");
+    return 0;
+}
