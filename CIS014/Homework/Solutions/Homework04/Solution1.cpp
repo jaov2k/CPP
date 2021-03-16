3f5011dfd561be4bf20831a3bbba3c7a8c52cc46
Jose Ortiz <jose@jaov2k.com>
2021-03-16 15:57:34 -0700

    progress

--- CIS014/Homework/Solutions/Homework04/Solution1.cpp
+++ CIS014/Homework/Solutions/Homework04/Solution1.cpp
@@ -12,20 +12,32 @@ using namespace std;
  * FUNCTION SINGATURE:
  *     int getLongestLength(string str)
  */ 
-void getLongestLength(string str)
+int getLongestLength(string str)
 {
+    const int size = 256;    
+    int tally[size] = {0};
+    int total = 0;
+
     for (char i : str)
+        tally[i]++;
+    
+    char c;
+    for (int i = 0; i < size; i++)
     {
-        cout << i << endl;     
+        if (tally[i] >= 1)
+        { 
+            c = i;
+            cout << "[" << i << "]:\t" << c << ":\t" << tally[i] << endl;
+        }
     }
-    cout << str << endl;
-    //return 0;
+    cout <<"String length is: " << str.length() << "\n\n" << endl;
+    return 0;
 }
 
 int main()
 {
     // your target function will be tested as such, 
-    // with a random input 
-    getLongestLength("abfdsafdsadfsaf");
+    // with a random input
+    cout << getLongestLength("aaaaAAAAbbbbbBBBccccccCCCdddddddDDDDeeeeeeeeEEEZx");
     return 0;
 }
