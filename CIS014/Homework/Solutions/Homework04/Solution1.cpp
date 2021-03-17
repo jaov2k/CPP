a07233237230f054be1e2c62f91773a20b6e5247
Jose Ortiz <jose@jaov2k.com>
2021-03-16 19:05:20 -0700

    finished assignment

--- CIS014/Homework/Solutions/Homework04/Solution1.cpp
+++ CIS014/Homework/Solutions/Homework04/Solution1.cpp
@@ -1,3 +1,10 @@
+/*
+Jose Ortiz
+3/16/2021
+CIS014 - Spring 2021
+Homework Assignment 4: Problem 1 
+*/
+
 // STARTER CODE
 #include <iostream>
 #include <string>
@@ -5,10 +12,12 @@ using namespace std;
 
 /**
  * FUNCTION NAME: getLongestLength
- * PURPOSE: 
+ * PURPOSE: To determine the largest possible palindrome given
+ *          a string of characters, a-z or A-Z.
  * PARAMETER:
  *     string str
  * RETURN VALUE:
+ *     int
  * FUNCTION SINGATURE:
  *     int getLongestLength(string str)
  */ 
@@ -17,27 +26,35 @@ int getLongestLength(string str)
     const int size = 256;    
     int tally[size] = {0};
     int total = 0;
-
+    bool singles = false;
+    
+    //Frequency histogram of the letters
     for (char i : str)
         tally[i]++;
     
-    char c;
+    //Totaling max size of the palindrome
     for (int i = 0; i < size; i++)
     {
-        if (tally[i] >= 1)
-        { 
-            c = i;
-            cout << "[" << i << "]:\t" << c << ":\t" << tally[i] << endl;
+        if (tally[i] > 1)
+        {
+            //if its even
+            if ((tally[i] % 2) == 0)
+                total += tally[i];
+            //if its odd
+            else if ((tally[i] % 2 ) != 0)
+                total = --tally[i];
         }
+        //if there's single instances
+        else if (tally[i] == 1)
+            singles = true;
     }
-    cout <<"String length is: " << str.length() << "\n\n" << endl;
-    return 0;
+    return singles ? ++total : total;
 }
 
 int main()
 {
-    // your target function will be tested as such, 
+    // your target function will be tested as such,
     // with a random input
-    cout << getLongestLength("aaaaAAAAbbbbbBBBccccccCCCdddddddDDDDeeeeeeeeEEEZx");
+    cout << getLongestLength("a");
     return 0;
 }
