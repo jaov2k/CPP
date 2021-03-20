69ad6374010044c85eda8a2eb28a10d432b2b906
Jose Ortiz <jose@jaov2k.com>
2021-03-20 13:41:22 -0700

    init commit

---
+++ CIS014/Homework/Homework05/Solution1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// YOUR API DOCUMENTATION HERE: FAILURE TO
+// INCLUDE API WILL BE DOCKED A POINT
+bool canPlantAvocados(vector<int> field, int n) {
+   // YOUR CODE HERE
+}
+int main() {
+   // your target function will be tested as such, 
+   // with random input
+   vector<int> field[] = {1,0,1};
+   cout << canPlantAvocados(field, 1); // returns 0
+   return 0;
+}
---
+++ CIS014/Homework/Homework05/Solution2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+// YOUR API DOCUMENTATION HERE: FAILURE TO
+// INCLUDE API WILL BE DOCKED A POINT
+int compareVersions(string ver1, string ver2) {
+   // YOUR CODE HERE
+}
+int main() {
+   // your target function will be tested as such, with 
+   // random input. To streamline your testing, you may
+   // also test your own code this way:
+   // cout << (compareVersions("0.1", "0.1") == 0); // 1, which is true
+   cout << compareVersions("0.1", "0.1"); // 0
+   return 0;
+}
