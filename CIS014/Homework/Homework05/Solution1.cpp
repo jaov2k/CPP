4fb3cfd0f3a8f424703294173932c377916346e7
Jose Ortiz <jose@jaov2k.com>
2021-03-22 11:19:34 -0700

    working on it

--- CIS014/Homework/Homework05/Solution1.cpp
+++ CIS014/Homework/Homework05/Solution1.cpp
@@ -1,16 +1,44 @@
 #include <iostream>
 #include <vector>
+
 using namespace std;
 
-// YOUR API DOCUMENTATION HERE: FAILURE TO
-// INCLUDE API WILL BE DOCKED A POINT
-bool canPlantAvocados(vector<int> field, int n) {
-   // YOUR CODE HERE
+/**
+ * PURPOSE: To test whether the number of avocados desired 
+ *          can be planted in the given field configuration
+ * PARAMETERS: vector, int
+ * RETURN VALUES: bool
+ * FUNCTION SINGATURE: bool canPlantAvocados(vector<int> field, int n)
+ */
+bool canPlantAvocados(vector<int> field, int n)
+{
+   bool isGoodField = false;
+   int goodSpaces = 0;
+   
+   // Count total available good spaces: [0, 0, 0]
+   for (int i = 1; i < n; i++)
+   {
+      if (field[i-1] == 0 && field[i] == 0 && field[i+1] == 0)
+      {
+         goodSpaces++;
+         n--;
+      }
+   }
+   if (goodSpaces <= n)
+      isGoodField = true;
+   
+   return isGoodField;
 }
+
+bool isGoodSpace(vector<int> temp)
+{
+   
+}
+
 int main() {
    // your target function will be tested as such, 
    // with random input
-   vector<int> field[] = {1,0,1};
+   vector<int> field = {1, 0, 0, 0, 0, 1, 0, 0, 1};
    cout << canPlantAvocados(field, 1); // returns 0
    return 0;
 }
