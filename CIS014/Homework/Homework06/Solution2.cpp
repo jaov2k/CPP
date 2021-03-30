9fedc9f7ef9bc9ff63fe308ce399d19e8bdc4f5e
Jose Ortiz <jose@jaov2k.com>
2021-03-30 13:53:57 -0700

    final commit

--- CIS014/Homework/Homework06/Solution2.cpp
+++ CIS014/Homework/Homework06/Solution2.cpp
@@ -3,15 +3,28 @@
 using namespace std;
 
 /**
- * PURPOSE:
- * PARAMETERS:
- * RETURN VALUES:
- * FUNCTION SINGATURE:
+ * PURPOSE: To simulate --N by taking in a vector representation as the function parameter.
+            The function returns the result of --N in its vector form
+ * PARAMETERS: vector<int>
+ * RETURN VALUES: vector<int>
+ * FUNCTION SINGATURE: vector<int> minusMinusN(vector<int> digits)
  */
 vector<int> minusMinusN(vector<int> digits) {
-   cout << digits.size();
-   
+   for (int i = digits.size() - 1; i >= 0; i--) {
+      if (digits[i] == 0)
+         digits[i] = 9;
+      else {
+         --digits[i];
+         if (digits[0] == 0 && digits.size() > 1) {
+            digits[0] = 9;
+            digits.pop_back();
+         }
+         break;
+      }
+   }
+   return digits;
 }
+
 int main() {
    // your target function will be tested like so, with random input
    vector<int> v {1,0};
