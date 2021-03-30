5b59011dcb2b8e40657b061699c6fd2c1f18870d
Jose Ortiz <jose@jaov2k.com>
2021-03-29 20:39:36 -0700

    init commit

---
+++ CIS014/Homework/Homework06/Solution2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+/**
+ * PURPOSE:
+ * PARAMETERS:
+ * RETURN VALUES:
+ * FUNCTION SINGATURE:
+ */
+vector<int> minusMinusN(vector<int> digits) {
+   // YOUR CODE HERE
+}
+int main() {
+   // your target function will be tested like so, with random input
+   vector<int> v {1,0};
+   vector <int> retVal = minusMinusN(v); // retVal = [9]
+   // etc.
+   return 0;
+}
