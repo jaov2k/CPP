589a5043e276fe6f826e4fe5fd12d86bf9863269
Jose Ortiz <jose@jaov2k.com>
2021-03-31 10:06:45 -0700

    init commit

---
+++ CIS014/Homework/Homework07/Solution1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+/**
+ * PURPOSE:
+ * PARAMETERS:
+ * RETURN VALUES:
+ * FUNCTION SIGNATURE:
+ */
+int getMaxProfit(vector<int> &prices) {
+   // YOUR CODE HERE
+}
+int main() {
+   // your target function will be tested as such, with random input like so
+   vector<int> arr{1, 2, 4};
+   cout << getMaxProfit(arr); // 3
+   return 0;
+}
