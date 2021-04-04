feb9b60a7df59346e16cd7881b5f5be452161bde
Jose Ortiz <jose@jaov2k.com>
2021-04-04 11:20:38 -0700

    updates

--- CIS014/Homework/Homework07/Solution2.cpp
+++ CIS014/Homework/Homework07/Solution2.cpp
@@ -3,17 +3,32 @@
 using namespace std;
 
 /**
- * PURPOSE:
- * PARAMETERS:
- * RETURN VALUES:
- * FUNCTION SIGNATURE:
+ * PURPOSE: Suggests the maximum profit an investor can make by placing AS MANY 
+ *          buy-sell orders in the given time slice the input vector represents.
+ * PARAMETERS: vector<int>
+ * RETURN VALUES: int
+ * FUNCTION SIGNATURE: int getMaxProfit(vector<int> &prices)
  */
 int getMaxProfit(vector<int> &prices) {
-   // YOUR CODE HERE
+   int output = 0, temp = 0;
+   
+   //First check if there is enough prices to perform a transaction.
+   if (prices.size() == 1)
+      return 0;
+   
+   //Search for the largest profit; Sell price is on the left, Buy price on the right.
+   for (int i = prices.size() - 1; i >=0; i--){
+      for (int j = i; j >=0; j--){
+         temp = prices[i] - prices[j];
+         if (temp > output)
+            output = temp; // Keep track of largest difference thus far.
+      }
+   }
+   return output; //Largest profit.
 }
 int main() {
    // your target function will be tested as such, with random input like so
-   vector<int> arr{3, 1, 5, 2, 4};
-   cout << getMaxProfit(arr); // 6
+   vector<int> arr{1, 2, 4};
+   cout << getMaxProfit(arr); // 3
    return 0;
 }
