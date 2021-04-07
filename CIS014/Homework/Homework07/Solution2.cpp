26a7bad576172b59e044c847b6c314291c613c10
Jose Ortiz <jose@jaov2k.com>
2021-04-07 11:32:14 -0700

    final commit

--- CIS014/Homework/Homework07/Solution2.cpp
+++ CIS014/Homework/Homework07/Solution2.cpp
@@ -10,25 +10,30 @@ using namespace std;
  * FUNCTION SIGNATURE: int getMaxProfit(vector<int> &prices)
  */
 int getMaxProfit(vector<int> &prices) {
-   int output = 0, temp = 0;
-   
+   int total = 0;
+
    //First check if there is enough prices to perform a transaction.
    if (prices.size() == 1)
       return 0;
    
-   //Search for the largest profit; Sell price is on the left, Buy price on the right.
-   for (int i = prices.size() - 1; i >=0; i--){
-      for (int j = i; j >=0; j--){
-         temp = prices[i] - prices[j];
-         if (temp > output)
-            output = temp; // Keep track of largest difference thus far.
-      }
+   //Total all the profits; Sell price is on the left, Buy price on the right.
+   for (int i = 0; i < prices.size() - 1; i++){
+      if(prices[i] < prices[i + 1])
+         total += prices[i + 1] - prices[i];
    }
-   return output; //Largest profit.
+   return total;
 }
 int main() {
    // your target function will be tested as such, with random input like so
    vector<int> arr{1, 2, 4};
-   cout << getMaxProfit(arr); // 3
+   cout << getMaxProfit(arr) << endl; // 3
+   arr = {4,2,1};
+   cout << getMaxProfit(arr) << endl; // 0
+   arr = {1};
+   cout << getMaxProfit(arr) << endl; // 0
+   arr = {1,2,5,1,6};
+   cout << getMaxProfit(arr) << endl; // 9
+   arr = {3,1,5,2,4};
+   cout << getMaxProfit(arr) << endl; // 6
    return 0;
 }
